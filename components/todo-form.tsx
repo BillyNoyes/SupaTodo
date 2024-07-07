fd24db21c5c78b3b6a0041cd5103bcc5151f327b
Billy Noyes <billy.noyes@wemakewebsites.com>
2024-07-07 02:51:30 +0100

    FEATURE: Implemented useFormStatus

--- components/todo-form.tsx
+++ components/todo-form.tsx
@@ -6,8 +6,11 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { Send } from "lucide-react";
 import { useRef } from "react";
+import { useFormStatus } from "react-dom";
 
 function FormContent() {
+  const { pending } = useFormStatus();
+
   return (
     <>
       <Textarea
@@ -15,10 +18,20 @@ function FormContent() {
         name="todo"
         required
         placeholder="Add a new todo"
+        disabled={pending}
       />
-      <Button type="submit" size="icon" className="min-w-10">
+
+      <Button
+        type="submit"
+        size="icon"
+        className="min-w-10"
+        disabled={pending}
+      >
         <Send className="h-5 w-5" />
-        <span className="sr-only">Submit Todo</span>
+
+        <span className="sr-only">
+          Submit Todo
+        </span>
       </Button>
     </>
   );
--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -7,6 +7,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { Todo } from "@/types/custom";
 import { Trash2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
 
 export function TodoItem({ todo }: { todo: Todo }) {
   return (
@@ -17,12 +18,15 @@ export function TodoItem({ todo }: { todo: Todo }) {
 }
 
 export function TodoCard({ todo }: { todo: Todo }) {
+  const { pending } = useFormStatus();
+
   return (
-    <Card className={cn("w-full")}>
+    <Card className={cn("w-full", pending && "opacity-50")}>
       <CardContent className="flex items-start gap-3 p-3">
         <span className="size-10 flex items-center justify-center">
           <Checkbox
             checked={Boolean(todo.is_complete)}
+            disabled={pending}
             onCheckedChange={async (val) => {
               if (val === 'indeterminate') return
 
@@ -41,6 +45,7 @@ export function TodoCard({ todo }: { todo: Todo }) {
           }}
           variant="ghost"
           size="icon"
+          disabled={pending}
         >
           <Trash2 className="h-5 w-5" />
 
