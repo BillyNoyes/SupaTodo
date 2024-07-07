c2625527640a6dfad6b0cd268ea71fe574921fc6
Billy Noyes <billy.noyes@wemakewebsites.com>
2024-07-07 02:47:49 +0100

    FEATURE: Implementing update and delete for todo tasks

--- app/todos/actions.ts
+++ app/todos/actions.ts
@@ -1,10 +1,11 @@
 'use server'
 
+import { Todo } from "@/types/custom";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
 export async function addTodo(formData: FormData) {
-  const supabase = createClient();
+  const supabase = createClient()
 
   const text = formData.get('todo') as string | null
 
@@ -25,6 +26,46 @@ export async function addTodo(formData: FormData) {
 
   if (error) {
     throw new Error('Error adding task')
+  }
+
+  revalidatePath('/todos')
+}
+
+export async function deleteTodo(id: number) {
+  const supabase = createClient()
+  const { data: { user }} = await supabase.auth.getUser()
+
+  if (!user) {
+    throw new Error('User is not logged in')
+  }
+
+  const { error } = await supabase.from('todos').delete().match({
+    user_id: user.id,
+    id: id
+  })
+
+  if (error) {
+    throw new Error('Error deleting task')
+  }
+
+  revalidatePath('/todos')
+}
+
+export async function updateTodo(todo: Todo) {
+  const supabase = createClient()
+  const { data: { user }} = await supabase.auth.getUser()
+
+  if (!user) {
+    throw new Error('User is not logged in')
+  }
+
+  const { error } = await supabase.from('todos').update(todo).match({
+    user_id: user.id,
+    id: todo.id
+  })
+
+  if (error) {
+    throw new Error('Error updating task')
   }
 
   revalidatePath('/todos')
